3cf579a8f4a1b91ca4c2bbb22ffb737b8295c5ad
paladinalessandro <paladinalessandro@gmail.com>
2024-08-02 22:35:23 +0200

    Swagger

--- src/app.ts
+++ src/app.ts
@@ -3,41 +3,48 @@ dotenv.config();
 import express, { Express, Request, Response } from "express";
 import { getAllEVPlugs, getCloseEVPlugs } from "./opendata";
 import { plug, plugRaw } from "./types";
+import swaggerUi from "swagger-ui-express";
+import swaggerOutput from "./swagger_output.json";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerOutput));
 
 app.get("/destinations", async (req: Request, res: Response) => {
   const query = req.query;
+  //INPUT DESTINATION + AUTONOMY LEFT
   if(query.latitude && query.longitude && query.distance_meters){
+    //getting query string params
     const lat:number = +query.latitude;
     const long:number = +query.longitude;
     const distance_meters = + query.distance_meters;
+    //params are not well formatted
     if(Number.isNaN(lat) || Number.isNaN(long)){
-      res.status(403).send("Bad request, number not formatted");
+      res.status(400).send("Bad request, number not formatted");
       return;
     }
     if(!Number.isNaN(lat) && !Number.isNaN(long) ){
-      const plugsRaw:plugRaw[]= await getCloseEVPlugs(lat,long,distance_meters);
-      let plugs:plug[]=[];
-      plugsRaw.forEach(plugRaw => {
-        plugs.push(
-          {uuid:plugRaw.scode,
-            roaDistance: 1,
-            outletType: plugRaw.smetadata.outlets[0].outletTypeCode,
-            powerWatt: plugRaw.smetadata.outlets[0].maxPower,
-            reviews: 1,
-            cost: 1}
-        );
-      });
-      res.json(plugs);
-      return;
+      try {
+        const plugsRaw:plugRaw[]= await getCloseEVPlugs(lat,long,distance_meters);
+        let plugs:plug[]=[];
+        plugsRaw.forEach(plugRaw => {
+          plugs.push(
+            {uuid:plugRaw.scode,
+              roaDistance: 1,
+              outletType: plugRaw.smetadata.outlets[0].outletTypeCode,
+              powerWatt: plugRaw.smetadata.outlets[0].maxPower,
+              reviews: 1,
+              cost: 1}
+          );
+        });
+        res.json(plugs);
+        return;
+      } catch (error) {
+        res.status(500).send("Internal server error");
+      }
     }
   }
   else{
@@ -47,20 +54,24 @@ app.get("/destinations", async (req: Request, res: Response) => {
 });
 
 app.get("/getAll", async (req: Request, res: Response) => {
-  const plugsRaw:plugRaw[]= await getAllEVPlugs();
-  let plugs:plug[]=[];
-  plugsRaw.forEach(plugRaw => {
-    plugs.push(
-      {uuid:plugRaw.scode,
-        roaDistance: 1,
-        outletType: plugRaw.smetadata.outlets[0].outletTypeCode,
-        powerWatt: plugRaw.smetadata.outlets[0].maxPower,
-        reviews: 1,
-        cost: 1}
-    );
-  });
-  res.json(plugs);
-  return;
+  try {
+    const plugsRaw:plugRaw[]= await getAllEVPlugs();
+    let plugs:plug[]=[];
+    plugsRaw.forEach(plugRaw => {
+      plugs.push(
+        {uuid:plugRaw.scode,
+          roaDistance: 1,
+          outletType: plugRaw.smetadata.outlets[0].outletTypeCode,
+          powerWatt: plugRaw.smetadata.outlets[0].maxPower,
+          reviews: 1,
+          cost: 1}
+      );
+    });
+    res.json(plugs);
+    return;
+  } catch (error) {
+    res.status(500).send("Internal server error");
+  }
 });
 
 app.listen(port, () => {
---
+++ src/swagger.ts
@@ -0,0 +1,28 @@
+import swaggerAutogen from 'swagger-autogen';
+
+const doc = {
+    info: {
+        version: 'v1.0.0',
+        title: 'Supa-Charging',
+        description: 'APIs to interact with open data & make calculations considering distance, cost, recharge speed of multiple EVstations plugs'
+    },
+    servers: [
+        {
+            url: 'http://localhost:8080',
+            description: ''
+        },
+    ],
+    components: {
+        securitySchemes: {
+            bearerAuth: {
+                type: 'http',
+                scheme: 'bearer',
+            }
+        }
+    }
+};
+
+const outputFile = './swagger_output.json';
+const endpointsFiles = ['./src/app.ts'];
+
+swaggerAutogen({openapi: '3.0.0'})(outputFile, endpointsFiles, doc);
