4a32d33245cc9fcd40eb1805009ba53f8d71deac
paladinalessandro <paladinalessandro@gmail.com>
2024-08-02 20:53:47 +0200

    Took out dist

--- dist/coordinates.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAirDistance = getAirDistance;
-const geolib_1 = require("geolib");
-function getAirDistance(cord1, cord2) {
-    return (0, geolib_1.getDistance)(cord1, cord2) / 1000;
-}
